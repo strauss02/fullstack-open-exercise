403ef8521d9692fac2e511d268530d086c6ec4c3
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-07 17:19:44 +0200

    #3.2 - GET request - /info

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 const path = require("path");
 const fs = require("fs");
+const moment = require("moment");
 
 app.use(cors({
     origin: "*"
@@ -17,12 +18,21 @@ app.use(express.json()) // parses requests as json
     response.send('<h1>Hello World!</h1>')
   })
   
-  //Get request for phoneBook data (3.1)
-  app.get('/api/persons', (request, response) => {
-      const dataFilePath = path.resolve(__dirname, "./phoneBook.json");
-      const phoneBookData = JSON.parse(fs.readFileSync(dataFilePath));
-      response.json(phoneBookData);
-  })
+//Get request for phoneBook data (3.1)
+app.get('/api/persons', (request, response) => {
+    const dataFilePath = path.resolve(__dirname, "./phoneBook.json");
+    const phoneBookData = JSON.parse(fs.readFileSync(dataFilePath));
+    response.json(phoneBookData);
+})
+
+//Get request for phoneBook info & request time (3.2)
+app.get('/info', (request, response) => {
+    const dataFilePath = path.resolve(__dirname, "./phoneBook.json");
+    const phoneBookData = JSON.parse(fs.readFileSync(dataFilePath));
+    
+    response.send(`<p>PhoneBook has info for ${phoneBookData.length} people</p>
+    <p>${moment().format('llll')}</p>`);
+})
   
   
   app.listen(PORT, () => {
