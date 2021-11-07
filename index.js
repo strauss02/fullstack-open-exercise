44044a85ab7786cdd9a0dab8aa01fa09931def6f
AmitFikler <amitfikler@gmail.com>
2021-11-07 14:31:44 +0200

    nodemon + express

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require("express")
+const app  = express()
+
+
 let notes = [
     {
       id: 1,
@@ -20,11 +23,17 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end(JSON.stringify(notes))
-})
 
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+  
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+  
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
