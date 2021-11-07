1ffe26b217d2609555444c253942bc8752c7d78f
Ido Strauss <strauss02@hotmail.co.il>
2021-11-07 16:08:31 +0200

    add server methods

--- index.js
+++ index.js
@@ -47,3 +47,32 @@ app.post('/api/notes', (req, res) => {
   res.json(note)
 })
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  console.log(id)
+  const note = notes.find((note) => note.id === id)
+
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter((note) => note.id !== id)
+
+  res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => console.log('server running on ', PORT))
