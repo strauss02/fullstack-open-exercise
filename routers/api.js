ae08e446d5f9f4bd07688d3c87cf03f983649e20
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-07 17:39:24 +0200

    #3.3 GET request - /api/persons/:id
    Implement the functionality for displaying the information for a single phonebook entry. The url for getting the data for a person with the id 5 should be http://localhost:3001/api/persons/5
    
    If an entry for the given id is not found, the server has to respond with the appropriate status code.

--- routers/api.js
+++ routers/api.js
@@ -13,6 +13,20 @@ router.get('/persons', (req, res) => {
     res.json(phoneBookData);
 })
 
+//Get request for phoneBook person data (3.3)
+router.get('/persons/:id', (req, res) => {
+    const reqId = req.params.id;
+    const dataFilePath = path.resolve(__dirname, "../phoneBook.json");
+    const phoneBookData = JSON.parse(fs.readFileSync(dataFilePath));
+    for (const person of phoneBookData) {
+        if (Number(person.id) === Number(reqId)) {
+            res.json(person);
+        }
+    }
+    //TODO - error handler & throw error here
+    res.status(400).send(`Error : bad request - no person with ID ${reqId}`);
+})
+
 
 
 module.exports = router;
