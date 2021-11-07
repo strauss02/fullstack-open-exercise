e4a13a0e35e13c6ae52ff384b109281b2811d85b
AmitFikler <amitfikler@gmail.com>
2021-11-07 15:19:13 +0200

    Fetching a single resource

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express")
 const express = require("express")
 const app  = express()
 
@@ -31,6 +32,16 @@ app.get('/', (request, response) => {
   
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+})
+
+app.get("/api/notes/:id", (request,response)=>{
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if(note){
+        response.json(note)
+    } else{
+        response.status(404).end()
+    }
 })
   
 const PORT = 3001
