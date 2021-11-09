58453b989ed85d746143a4190bd2e1ee4d559a4c
AmitFikler <amitfikler@gmail.com>
2021-11-09 11:00:22 +0200

    deployment

--- Notes/Front/index.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = 'http://localhost:3001/api/notes'
-
-async function getAll(){
-    const request = await axios.get(baseUrl)
-    console.log(request.data)
-}
-
-
-document.getElementById("try").addEventListener("click",getAll)
