77625c7d0f8e0b3117e51d9a531ca3591e1fb900
AmitFikler <amitfikler@gmail.com>
2021-11-09 11:10:39 +0200

    deployment

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use("/", express.static(`./Front`));
 app.get("/", (req, res) => {
-  res.sendFile("../Front/index.html");
+  res.sendFile("/A Phonebook/Front/index.html");
 });
 
 
