8217daf805ad54f074d1338bacd3cd1368c60c2c
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-10 20:23:55 +0200

    # Auto Code Style changes

--- back-end/models/contact.js
+++ back-end/models/contact.js
@@ -1,31 +1,25 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const contactsSchema = new mongoose.Schema({
-    // _id: {
-    //   type: Number, 
-    //   required: true, 
-    //   unique: true
-    // },
-    name: {
-      type: String, 
-      required: true, 
-      unique: true
-    },
-    number: {
-      type: String,
-      required: true
-    },
-})
+  name: {
+    type: String,
+    required: true,
+    unique: true,
     minLength: 3,
+  },
+  number: {
+    type: String,
+    required: true,
     minLength: 8,
+  },
+});
 
-contactsSchema.set('toJSON', {
+contactsSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Contact', contactsSchema);
+module.exports = mongoose.model("Contact", contactsSchema);
